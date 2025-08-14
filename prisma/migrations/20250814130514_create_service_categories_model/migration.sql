adc43b53d48fd3e090471195a6c7517320ea93c1
lutfiya ainurrahman <lutfiyapr.creative@gmail.com>
2025-08-14 20:06:13 +0700

    feat: add service categories model

---
+++ prisma/migrations/20250814130514_create_service_categories_model/migration.sql
@@ -0,0 +1,48 @@
+/*
+  Warnings:
+
+  - You are about to drop the `prescriptions_items` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE `prescriptions_items` DROP FOREIGN KEY `prescriptions_items_medicine_id_fkey`;
+
+-- DropForeignKey
+ALTER TABLE `prescriptions_items` DROP FOREIGN KEY `prescriptions_items_prescription_id_fkey`;
+
+-- DropTable
+DROP TABLE `prescriptions_items`;
+
+-- CreateTable
+CREATE TABLE `prescription_items` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `prescription_id` INTEGER NOT NULL,
+    `medicine_id` INTEGER NOT NULL,
+    `dosage` VARCHAR(64) NULL,
+    `frequency` VARCHAR(64) NULL,
+    `duration_days` INTEGER NULL,
+    `instructions` TEXT NULL,
+    `created_at` DATETIME(3) NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `service_categories` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(64) NOT NULL,
+    `description` TEXT NULL,
+    `price` DECIMAL(10, 2) NOT NULL,
+    `is_active` BOOLEAN NOT NULL DEFAULT true,
+    `created_at` DATETIME(3) NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `prescription_items` ADD CONSTRAINT `prescription_items_prescription_id_fkey` FOREIGN KEY (`prescription_id`) REFERENCES `prescriptions`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `prescription_items` ADD CONSTRAINT `prescription_items_medicine_id_fkey` FOREIGN KEY (`medicine_id`) REFERENCES `medicines`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
