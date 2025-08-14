54f4b60d1af96f4ab85573673fc0ef45317d7cb1
lutfiya ainurrahman <lutfiyapr.creative@gmail.com>
2025-08-14 19:57:36 +0700

    feat: add prescription items model

---
+++ prisma/migrations/20250814125441_create_prescriptions_items_model/migration.sql
@@ -0,0 +1,20 @@
+-- CreateTable
+CREATE TABLE `prescriptions_items` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `prescription_id` INTEGER NOT NULL,
+    `medicine_id` INTEGER NOT NULL,
+    `dosage` VARCHAR(64) NULL,
+    `frequency` VARCHAR(64) NULL,
+    `duration_days` INTEGER NULL,
+    `instructions` TEXT NULL,
+    `created_at` DATETIME(3) NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `prescriptions_items` ADD CONSTRAINT `prescriptions_items_prescription_id_fkey` FOREIGN KEY (`prescription_id`) REFERENCES `prescriptions`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `prescriptions_items` ADD CONSTRAINT `prescriptions_items_medicine_id_fkey` FOREIGN KEY (`medicine_id`) REFERENCES `medicines`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
