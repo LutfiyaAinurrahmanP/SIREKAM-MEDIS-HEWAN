f3620810e5f203ba61a2ceefbf3f3085e769a9d4
lutfiya ainurrahman <lutfiyapr.creative@gmail.com>
2025-09-15 07:34:05 +0700

    feat: configuration cors for backend

--- src/backend/application/web.ts
+++ src/backend/application/web.ts
@@ -2,10 +2,12 @@ import express from "express";
 import { guestRouter } from "../route/guest";
 import { errorValidationMiddleware } from "../middleware/error-validation-middleware";
 import { apiRouter } from "../route/api";
-import { authMiddleware } from "../middleware/auth-middleware";
+// import { authMiddleware } from "../middleware/auth-middleware";
+import cors from "cors";
 
 export const web = express();
 web.use(express.json());
+web.use(cors());
 web.use(guestRouter);
 web.use(apiRouter);
 web.use(errorValidationMiddleware);
