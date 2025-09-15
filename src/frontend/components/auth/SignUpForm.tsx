6fd95c525cc8e0aec775cd4eeb6bbe2b9dfa7622
lutfiya ainurrahman <lutfiyapr.creative@gmail.com>
2025-09-15 08:53:44 +0700

    feat: update auth design

--- src/frontend/components/auth/SignUpForm.tsx
+++ src/frontend/components/auth/SignUpForm.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router";
-import { ChevronLeftIcon, EyeCloseIcon, EyeIcon } from "../../icons";
+import { EyeCloseIcon, EyeIcon } from "../../icons";
 import Label from "../form/Label";
 import Input from "../form/input/InputField";
 import Checkbox from "../form/input/Checkbox";
@@ -9,54 +9,56 @@ export default function SignUpForm() {
   const [showPassword, setShowPassword] = useState(false);
   const [isChecked, setIsChecked] = useState(false);
 
-  // Send data
-  const [username, setUsername] = useState("");
-  const [fullName, setFullname] = useState("");
-  const [semail, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [role, setRole] = useState("");
-  const [phone, setPhone] = useState("");
-  
   return (
     <div className="flex flex-col flex-1 w-full overflow-y-auto lg:w-1/2 no-scrollbar">
       <div className="flex flex-col justify-center flex-1 w-full max-w-md mx-auto">
         <div>
           <div className="mb-5 sm:mb-8">
             <h1 className="mb-2 font-semibold text-gray-800 text-title-sm dark:text-white/90 sm:text-title-md">
-              Sign Up
+              Register
             </h1>
             <p className="text-sm text-gray-500 dark:text-gray-400">
-              Enter your email and password to sign up!
+              Enter your data to register!
             </p>
           </div>
           <div>
             <form>
               <div className="space-y-5">
-                <div className="grid grid-cols-1 gap-5 sm:grid-cols-2">
-                  {/* <!-- First Name --> */}
-                  <div className="sm:col-span-1">
-                    <Label>
-                      First Name<span className="text-error-500">*</span>
-                    </Label>
-                    <Input
-                      type="text"
-                      id="fname"
-                      name="fname"
-                      placeholder="Enter your first name"
-                    />
-                  </div>
-                  {/* <!-- Last Name --> */}
-                  <div className="sm:col-span-1">
-                    <Label>
-                      Last Name<span className="text-error-500">*</span>
-                    </Label>
-                    <Input
-                      type="text"
-                      id="lname"
-                      name="lname"
-                      placeholder="Enter your last name"
-                    />
-                  </div>
+                {/* <!-- Username --> */}
+                <div className="sm:col-span-1">
+                  <Label>
+                    Username<span className="text-error-500">*</span>
+                  </Label>
+                  <Input
+                    type="text"
+                    id="username"
+                    name="username"
+                    placeholder="Enter your username"
+                  />
+                </div>
+                {/* <!-- Full Name --> */}
+                <div className="sm:col-span-1">
+                  <Label>
+                    Fullname<span className="text-error-500">*</span>
+                  </Label>
+                  <Input
+                    type="text"
+                    id="fullname"
+                    name="fullname"
+                    placeholder="Enter your fullname"
+                  />
+                </div>
+                {/* <!-- Phone --> */}
+                <div className="sm:col-span-1">
+                  <Label>
+                    Phone<span className="text-error-500">*</span>
+                  </Label>
+                  <Input
+                    type="text"
+                    id="phone"
+                    name="phone"
+                    placeholder="Enter your phone number"
+                  />
                 </div>
                 {/* <!-- Email --> */}
                 <div>
@@ -113,7 +115,7 @@ export default function SignUpForm() {
                 {/* <!-- Button --> */}
                 <div>
                   <button className="flex items-center justify-center w-full px-4 py-3 text-sm font-medium text-white transition rounded-lg bg-brand-500 shadow-theme-xs hover:bg-brand-600">
-                    Sign Up
+                    Register
                   </button>
                 </div>
               </div>
@@ -123,10 +125,10 @@ export default function SignUpForm() {
               <p className="text-sm font-normal text-center text-gray-700 dark:text-gray-400 sm:text-start">
                 Already have an account? {""}
                 <Link
-                  to="/signin"
+                  to="/login"
                   className="text-brand-500 hover:text-brand-600 dark:text-brand-400"
                 >
-                  Sign In
+                  Login
                 </Link>
               </p>
             </div>
